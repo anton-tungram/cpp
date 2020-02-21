d6b7fd65c7703cfbe7e74f3139ea4d6a8425f22a
anton-tungram <60393924+anton-tungram@users.noreply.github.com>
2020-02-21 16:59:03 +0300

    Update world.cpp

--- world.cpp
+++ world.cpp
@@ -1,14 +1,6 @@
 #include <iostream>
 int main(int argc, char* argv[])
 {
-   int i = 10;
    std::cout<<"Hello World!"<<std::endl;
-   // Сколько раз???
-   while(i--)
-   {
-      // Сколько раз будет вызвана эта строка???/
-      for(int j=0; j<10; j++)
-         std::cout<<"Hello World!"<<std::endl;
-   }
    return 0;
 }
